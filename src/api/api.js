1cb40835ea17f7d981baf8167428d77483b75074
Pancho Fernandez <panchofern@gmail.com>
2022-04-30 17:47:59 -0400

    added last_updated

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const url = "https://fyeo-chatbot.herokuapp.com";
 
-// const url = "localhost:5000";
+// const url = "http://localhost:5000";
 
 export const api = axios.create({
   baseURL: url,
--- src/staff-side/FAQList.js
+++ src/staff-side/FAQList.js
@@ -149,6 +149,11 @@ const FAQList = () => {
             >
               <Card.Meta
                 style={{ marginBottom: 10 }}
+                title={
+                  <p style={{ fontSize: 14, fontWeight: "normal" }}>{`Last Updated: ${
+                    item.last_updated ? item.last_updated.split(" ").slice(0, 4).join(" ") : ""
+                  }`}</p>
+                }
                 description={item.patterns.map((p, i) => (
                   <Tag key={i} color="cyan" style={{ marginBottom: 5 }}>
                     {p}
