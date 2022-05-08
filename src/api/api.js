c7e0a9263653ab44d9999f4257e29d711efc8da6
Pancho Fernandez <panchofern@gmail.com>
2022-05-08 18:11:45 -0400

    changed api base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const url = "https://fyeo-chatbot.herokuapp.com";
+const url = "https://fyeo-chatbot.herokuapp.com";
 
-const url = "http://localhost:5000";
+// const url = "http://localhost:5000";
 
 export const api = axios.create({
   baseURL: url,
